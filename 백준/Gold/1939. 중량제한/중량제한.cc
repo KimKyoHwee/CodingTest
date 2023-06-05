9619f00b94fcb23745c75437f4c4ee73cfbcaf66
김교휘 <122682443+KimKyoHwee@users.noreply.github.com>
2023-06-06 02:28:46 +0900

    [Gold III] Title: 중량제한, Time: 68 ms, Memory: 7752 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.cc
+++ 백준/Gold/1939. 중량제한/중량제한.cc
@@ -1,71 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N,M;
-//vector<pair<int , int> > pArr[100001];  //pArr[a].{b,c}는 a와b 사이의 다리 가중치가 c  
-int root[100001]; // Kruskal용 부모 노드 가리키기. 
-int resultA, resultB;  //결과를 찾아야하는 A와 B노드 
+int N, M;
+vector<pair<int, int> > pArr[100001];  //pArr[a].{b,c}는 a와b 사이의 다리 가중치가 c
+int minE = 2100000000, maxE = 0;  // parametric 의 범위 
+int resultA, resultB;  //최종 목표 : resultA에서 resultB로 가기  
+bool visited[100001];  //bfs용 
 
-typedef struct node{   // Kruskal 사용하기 위하여 edge들의 내림차순 정렬 위해 구조체 선언 
-	int a, b, c;
-}Node;
-vector<Node> nodeV;            //구조체 벡터(Kruskal용) 
-
-bool compare(Node x, Node y){  //Kruskal edge내림차순 정렬용 비교함수  
-	return x.c>y.c;
-}
-
-void create_set(int n){  //union-set
-	for(int i=1;i<=n;i++) root[i]=i;
-}
-pair<int, int> find_set(int n){  // union-set (union함수에서 높이 필요하므로 {루트노드, 높이} 반환 
-	int height=1;
-	while(1){
-		int imsi=root[n];
-		if(imsi==n) return {n, height};
-		else {
-			n=imsi;
-			height++;
+bool promising(int mid) { //bfs  resultA에서 시작해서 넘겨받은 mid(가중치)로 resultB까지 갈수 있는지   
+	fill(&visited[0], &visited[100001], 0);
+	visited[resultA] = true; //resultA에서 시작
+	queue<int> iQ;
+	iQ.push(resultA);
+	while (!iQ.empty()) {
+		int node = iQ.front();
+		iQ.pop();
+		if (node == resultB) {  //resultB에 도착했을 경우 
+			return true;
+		}
+		for (int i = 0; i < pArr[node].size(); i++) {  //queue에서 뽑은 노드에서 갈수 있는 곳 다 가보기 
+			int nextN = pArr[node][i].first;  //갈 곳 노드 
+			int edgeN = pArr[node][i].second;  //갈 곳과 연결된 edge가중치
+			if (mid <= edgeN && (!visited[nextN])) {  //edge가 허용 가능범위고, 방문하지 않은 노드라면 queue에 다음 노드 넣기 
+				iQ.push(nextN);
+				visited[nextN] = true;
+			}
 		}
 	}
-}
-void union_set(int x, int y){  //union_set
-	pair<int, int> imsiA=find_set(x); 
-	pair<int, int> imsiB=find_set(y);
-	if(imsiA.second>=imsiB.second) root[imsiA.first]=imsiB.first;
-	else root[imsiB.first]=imsiA.first;
+	return false;
 }
 
+int parametric(int miny, int maxy) {
+	if (miny == maxy) return miny;
+	int mid = (miny + maxy) / 2;
+	if (promising(mid)) {
+		if (maxy - miny == 1) {
+			if (promising(maxy))return maxy;
+			else return miny;
+		}
+		return parametric(mid, maxy);
+	}
+	else {
+		return parametric(miny, mid - 1);
+	}
 
-int main(){
+}
+
+int main() {
+	
 	std::ios_base::sync_with_stdio(0);
 	std::cin.tie(0);
-	cin>>N>>M;
-	for(int i=0;i<M;i++){
-		int A,B,C;
-		cin>>A>>B>>C;
-		//pArr[A].push_back({B,C});
-		Node imsi;
-		imsi.a=A;
-		imsi.b=B;
-		imsi.c=C;
-		nodeV.push_back(imsi);
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
+		int A, B, C;
+		cin >> A >> B >> C;
+		if (C > maxE) maxE = C;
+		if (C < minE) minE = C;
+		pArr[A].push_back({ B,C });
+		pArr[B].push_back({ A,C });
 	}
-	cin>>resultA>>resultB;
-	//기본입력 끝
-	sort(nodeV.begin(),nodeV.end(), compare);  //Kruskal용 
-	//union-set시작 
-	create_set(N);
-	int nodeVcnt=0;
-	while(1){
-		int imsiA=nodeV[nodeVcnt].a;
-		int imsiB=nodeV[nodeVcnt].b;
-		union_set(imsiA, imsiB);
-		if(find_set(resultA).first==find_set(resultB).first){
-			cout<<nodeV[nodeVcnt].c;
-			return 0;
-		}
-		nodeVcnt++;
-	}
+	cin >> resultA >> resultB;
+	//초기 입력 끝 
+	cout<<parametric(0, maxE);
 	return 0;
 }
