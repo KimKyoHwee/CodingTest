e7886e1d396688981a3b46fae29db1331e025e1e
김교휘 <122682443+KimKyoHwee@users.noreply.github.com>
2024-05-19 00:48:19 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string str = "";
+	cin >> str;
+	int num = 0, ptr = 0;
+
+	while (num++ < 30000) {
+		string num2str = to_string(num);
+		for (int i = 0; i < num2str.length(); i++) {
+			// num의 요소 중 일치하는 것이 있으면 포인터 증가
+			if (str[ptr] == num2str[i]) {
+				ptr++;
+			}
+			if (ptr == str.length()) {
+				cout << num;
+				return 0;
+			}
+		}
+	}
+	return 0;
+}
