3faf0e11365989f8fc906bdb4b2e8c3701e860e8
김교휘 <122682443+KimKyoHwee@users.noreply.github.com>
2024-05-21 14:14:17 +0900

    [Silver III] Title: 타노스, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.cc
+++ 백준/Silver/20310. 타노스/타노스.cc
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 int zeroCnt, oneCnt;
 int main(){
@@ -8,10 +9,24 @@ int main(){
         if(s[i]=='0') zeroCnt++;
         else oneCnt++;
     }
-    string answer="";
-    for(int i=0;i<(zeroCnt/2);i++) answer+='0';
-    for(int i=0;i<(oneCnt/2);i++) answer+='1';
-    cout<<answer;
-
+    zeroCnt/=2;
+    oneCnt/=2;
+    vector<int> answerIndexV;
+    for(int i=s.size()-1;i>=0;i--){
+        if(!oneCnt) break;
+        if(s[i]=='1'){
+            answerIndexV.push_back(i);
+            oneCnt--;
+        }
+    }
+    for(int i=0;i<s.size();i++){
+        if(!zeroCnt) break;
+        if(s[i]=='0'){
+            answerIndexV.push_back(i);
+            zeroCnt--;
+        }
+    }
+    sort(answerIndexV.begin(), answerIndexV.end());
+    for(int i=0;i<answerIndexV.size();i++) cout<<s[answerIndexV[i]];
     return 0;
 }
