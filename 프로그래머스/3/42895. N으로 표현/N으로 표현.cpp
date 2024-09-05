c7bbae29bf11e44890d22a173bc1905fe368a51e
김교휘 <122682443+KimKyoHwee@users.noreply.github.com>
2024-09-06 01:28:52 +0900

    [level 3] Title: N으로 표현, Time: 12.67 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 using namespace std;
-
+/*
 int solution(int N, int number) {
     if (N == number) return 1; // N이 number와 같으면 N 한 번 사용
 
@@ -36,5 +36,29 @@ int solution(int N, int number) {
     }
 
     // 8번 사용했는데도 만들 수 없으면 -1 리턴
+    return -1;
+}
+*/
+int solution(int N, int number){
+    if (N == number) return 1;
+    vector<set<int>> dp(9);
+    int gop=N;
+    for(int i=1;i<=8;i++){
+        dp[i].insert(gop);
+        gop=gop*10+N;
+    }
+    for(int i=1;i<9;i++){
+        for(int j=1;j<i;j++){
+            for(int a:dp[j]){
+                for(int b:dp[i-j]){
+                    dp[i].insert(a+b);
+                    dp[i].insert(a-b);
+                    dp[i].insert(a*b);
+                    if(b!=0&&a!=0)dp[i].insert(a/b);
+                }
+            }
+        }
+        if(dp[i].find(number)!=dp[i].end()) return i;
+    }
     return -1;
 }
