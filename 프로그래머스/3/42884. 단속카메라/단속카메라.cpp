fdbe879c5d2c706fc31177d2b8822d117812af40
hwee <122682443+KimKyoHwee@users.noreply.github.com>
2024-10-11 19:03:11 +0900

    [level 3] Title: 단속카메라, Time: 0.48 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -1,28 +1,41 @@
 #include <string>
 #include <vector>
 #include <utility>
+#include <iostream>
 #include <algorithm>
 using namespace std;
-
+struct Route{
+    int start, end;
+    bool operator<(Route other) const{
+        return start<other.start;
+    }
+};
+Route makeRoute(int start, int end){
+    Route route;
+    route.start=start;
+    route.end=end;
+    return route;
+}
 int solution(vector<vector<int>> routes) {
-    // 차량 경로를 진출 시점을 기준으로 오름차순 정렬
-    sort(routes.begin(), routes.end(), [](vector<int> a, vector<int> b) {
-        return a[1] < b[1];
-    });
-
-    int answer = 0;
-    int camera_position = -30001;  // 카메라가 아직 설치되지 않았다고 가정
-    
-    for (int i = 0; i < routes.size(); i++) {
-        int nowA = routes[i][0];
-        int nowB = routes[i][1];
-
-        // 현재 카메라가 이 차량의 경로에 포함되지 않는다면 새로운 카메라 설치
-        if (camera_position < nowA) {
+    vector<Route> inputs;
+    for(int i=0;i<routes.size();i++){
+        Route route=makeRoute(routes[i][0], routes[i][1]);
+        inputs.push_back(route);
+    }
+    sort(inputs.begin(), inputs.end());
+    int lastCamera=-30001;
+    int answer=0;
+    for(int i=0;i<inputs.size();i++){
+        int nowStart=inputs[i].start;
+        int nowEnd=inputs[i].end;
+        if(nowStart<=lastCamera) {
+            if(nowEnd<=lastCamera) lastCamera=nowEnd;
+            continue;
+        }
+        else{
+            lastCamera=nowEnd;
             answer++;
-            camera_position = nowB;  // 새로운 카메라는 이 차량이 나가는 지점에 설치
         }
     }
-
     return answer;
 }
